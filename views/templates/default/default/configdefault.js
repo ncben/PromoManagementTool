de58bdca0b522575cf81e6852b611f00977d548c
Ben Wong <ncbenhk@hotmail.com>
2016-04-09 03:13:54 -0400

    Inital Commit

---
+++ views/templates/default/default/configdefault.js
@@ -0,0 +1,153 @@
+var config = {}
+
+config.essay = {
+	name : '../default/essay.html',
+	essaybox : 'textarea[role="essay"]',
+	essaycounter : 'span[role="counter"]',
+	js : '../default/essay.js'
+}
+config.inputText = {
+	name : '../default/inputText.html'
+}
+config.recaptcha = {
+	name : '../default/recaptcha.html'
+}
+config.checkbox = {
+	name : '../default/inputCheckbox.html'
+}
+config.image = {
+	name : '../default/image.html'
+}
+config.imageLink = {
+	name : '../default/imageLink.html'
+}
+config.text = {
+	name : '../default/text.html'
+}
+config.inputPhone = {
+	name: '../default/inputPhone.html'
+}
+config.inputRadio = {
+	name : '../default/inputRadio.html'
+}
+
+config.textarea = {
+	name: '../default/textarea.html'
+}
+
+config.inputStateDropDown = {
+	name: '../default/inputStateDropDown.html'
+}
+
+
+config.countryDropDown = {
+	name: '../default/countryDropDown.html'
+}
+
+config.dropDown = {
+	name : '../default/dropDown.html'
+}
+
+config.textLink = {
+	name: '../default/textLink.html'
+}
+
+config.submitButton = {
+	name: '../default/submitButton.html'
+}
+
+config.buttonLink = {
+	name: '../default/buttonLink.html'
+}
+
+config.photo = {
+	name: '../default/photo.html',
+	js : '../default/photo.js'
+}
+config.lineBreak = {
+	name: '../default/lineBreak.html'
+}
+
+config.slider = {
+	name : '../default/slider.html',
+	js : '../default/slider.js'
+}
+
+config.dobFull = {
+	name : '../default/dobFull.html'
+}
+config.likeButton = {
+	name: '../default/likeButton.html'
+}
+config.likeBox = {
+	name: '../default/likeBox.html'
+}
+
+config.sendButton = {
+	name: '../default/sendButton.html'
+}
+
+config.embeddedPost = {
+	name: '../default/embeddedPost.html'
+}
+
+config.followButton = {
+	name: '../default/followButton.html'
+}
+
+config.shareButton = {
+	name: '../default/shareButton.html'
+}
+
+config.appRequest = {
+	name: '../default/appRequest.html'
+}
+
+config.sendDialog = {
+	name: '../default/sendDialog.html'
+}
+
+config.feedDialog = {
+	name: '../default/feedDialog.html'
+}
+
+config.twitterShare = {
+	name: '../default/twitterShare.html'
+}
+
+config.googlePlusShare = {
+	name: '../default/googlePlusShare.html'
+}
+
+config.pinterestShare = {
+	name: '../default/pinterestShare.html'
+}
+
+config.linkedInShare = {
+	name: '../default/linkedInShare.html'
+}
+
+config.emailShare = {
+	name: '../default/emailShare.html'
+}
+
+config.commentBox = {
+	name: '../default/commentBox.html'
+}
+
+config.disqus = {
+	name: '../default/disqus.html'
+}
+
+config.prefillWithFacebook = {
+	name: '../default/prefillWithFacebook.html'
+}
+
+config.hiddenInput = {
+	name: '../default/hiddenInput.html'
+}
+
+
+
+
+module.exports = config;
